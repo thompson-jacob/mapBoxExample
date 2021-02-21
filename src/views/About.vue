66365193bf351feedb0d052f59f45d6c48ee4ab6
Jacob Thompson <Cobalt7291@gmail.com>
2021-02-21 11:33:28 -0500

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="map" style="width: 400px; height: 300px;"></div>
 </template>
+
+<script>
+/* global mapboxgl */
+export default {
+  data() {
+    return {};
+  },
+
+  mounted: function() {
+    this.setupMap();
+  },
+  methods: {
+    setupMap: function() {
+      // mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
+      mapboxgl.accessToken = "";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        // center: [40.74, -74.54],
+        zoom: 4,
+      });
+      console.log(map);
+    },
+  },
+};
+</script>
