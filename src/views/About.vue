58370531bebc1b73e0dbd69f12b568eabb432096
Jacob Thompson <Cobalt7291@gmail.com>
2021-02-21 14:00:25 -0500

    brackets

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,24 +5,36 @@
 <script>
 /* global mapboxgl */
 export default {
-  data() {
-    return {};
+  data: function()  {
+      places: [
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+        { lat: -33.8675, lng: 151.207, description: "The main city!" },
+      ],
   },
 
   mounted: function() {
+    places.forEach(function(i) {
+      console.log(i)
+    });
     this.setupMap();
   },
   methods: {
     setupMap: function() {
-      // mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
-      mapboxgl.accessToken = "";
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
       var map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
-        // center: [40.74, -74.54],
+        center: [-87.6298, 41.8781],
         zoom: 4,
       });
       console.log(map);
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText("You are Here");
+      var marker = new mapboxgl.Marker()
+        .setLngLat([-87.6298, 41.8781])
+        .setPopup(popup)
+        .addTo(map);
+      console.log(marker);
+      console.log(popup);
     },
   },
 };
